2cf0d867c3686aa1b5be04609c17d7a656fdd6bf
hrynevychroman <hrynevych.romann@gmail.com>
2024-10-24 09:46:22 +0300

    fix: remove unneded import `computed`

--- src/components/PracticalExamples.vue
+++ src/components/PracticalExamples.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref } from 'vue';
 import { format, addHours, addDays } from 'date-fns';
 import { TZDate } from '@date-fns/tz';
 
